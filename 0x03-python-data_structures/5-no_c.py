ef7710db7301401ea0cc4d4bd9fe4bc9e0ac8dd3
AAYMANO <aymanairout7@gmail.com>
2023-09-09 09:02:45 -0700

    lol

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    ret = ""
+    ret = " "
     for a in range(len(my_string)):
         if (my_string[a] != 'c' and my_string[a] != 'C'):
             ret += my_string [a]
