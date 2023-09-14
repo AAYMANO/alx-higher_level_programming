fa58d8fee9f7f8d28fe396afdf5c447f88895c7c
AAYMANO <aymanairout7@gmail.com>
2023-09-14 06:04:21 -0700

    lol

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -10,28 +10,36 @@ void print_python_bytes(PyObject *p)
 {
 	char *string;
 	long int size, a, limit;
+
 	printf("[.] bytes object info\n");
 	if (!PyBytes_Check(p))
 	{
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
+
 	size = ((PyVarObject *)(p))->ob_size;
 	string = ((PyBytesObject *)p)->ob_sval;
+
 	printf("  size: %ld\n", size);
 	printf("  trying string: %s\n", string);
+
 	if (size >= 10)
 		limit = 10;
 	else
 		limit = size + 1;
+
 	printf("  first %ld bytes:", limit);
+
 	for (a = 0; a < limit; a++)
-		if (stringai] >= 0)
+		if (string[a] >= 0)
 			printf(" %02x", string[a]);
 		else
 			printf(" %02x", 256 + string[a]);
+
 	printf("\n");
 }
+
 /**
  * print_python_list - Prints list information
  * @p: Python Object
@@ -42,11 +50,14 @@ void print_python_list(PyObject *p)
 	long int size, a;
 	PyListObject *list;
 	PyObject *obj;
+
 	size = ((PyVarObject *)(p))->ob_size;
 	list = (PyListObject *)p;
+
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %ld\n", size);
 	printf("[*] Allocated = %ld\n", list->allocated);
+
 	for (a = 0; a < size; a++)
 	{
 		obj = ((PyListObject *)p)->ob_item[a];
