dc38fd3a3dc5a6ba32e140dcd5bc4b0427f5ca1c
AAYMANO <aymanairout7@gmail.com>
2023-09-26 07:09:19 -0700

    lol

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -32,9 +32,8 @@ class Square:
 
     def my_print(self):
         """Print the square with the # character."""
-        for p in range(0, self.__size):
+        for i in range(0, self.__size):
             [print("#", end="") for j in range(self.__size)]
             print("")
         if self.__size == 0:
             print("")
-
